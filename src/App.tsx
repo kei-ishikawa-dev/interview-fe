d3b94ccdc94e14f6225c692b3862bf11bea9a39a
Ishikawa Kei <k.ishikawa.dev@gmail.com>
2026-03-09 15:13:03 +0900

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,55 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import type { Get, Article } from './types/get';
+import { Frame } from './components/frame';
+import { Create } from './components/create';
+import type { CreatePayload } from './types/create';
+
+const test: CreatePayload = {
+  title: 'タイトル',
+  body: 'テスト',
+};
+
 export default function App() {
-  return <div className="text-2xl">FE面接用</div>;
+  const [data, setData] = useState<Article[] | null>(null);
+
+  const post = async () => {
+    const response = await axios.post<CreatePayload, Article>(
+      'https://asia-northeast1-praha-test.cloudfunctions.net/createPost',
+      test,
+    );
+
+    setData([response, ...(data ?? [])]);
+  };
+  const handleClick = () => {
+    post();
+  };
+
+  const get = async () => {
+    const response = await axios.get<Get>(
+      'https://asia-northeast1-praha-test.cloudfunctions.net/getPosts',
+    );
+    setData(response.data.data);
+  };
+  useEffect(() => {
+    get();
+  }, []);
+
+  return (
+    <>
+      {data &&
+        data.map((x) => (
+          <Frame
+            key={x.id}
+            id={x.id}
+            body={x.body}
+            createdAt={x.createdAt}
+            createdBy={x.createdBy}
+            title={x.title}
+            isFavorite={x.isFavorite}
+          />
+        ))}
+      <Create onClick={handleClick} />
+    </>
+  );
 }
